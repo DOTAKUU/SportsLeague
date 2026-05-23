ff00b09281d4e26480fd60c26ae3ec686413494e
JOSE DANIEL LOPEZ PEREIRA <joselopez1130071@correo.itm.edu.co>
2026-05-23 04:53:04 -0500

    My local files updated

--- SportsLeague.Domain/Entities/Referee.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SportsLeague.Domain.Entities
-{
-    public class Referee : AuditBase
-    {
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public string Nationality { get; set; } = string.Empty;
-    }
-}
